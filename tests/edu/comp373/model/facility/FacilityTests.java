dd948701afae5a8fcff7c4d44d91de7f8a574486
John O'Sullivan <josullivan1@luc.edu>
2018-02-13 19:08:44 -0600

    Add Tests

---
+++ tests/edu/comp373/model/facility/FacilityTests.java
@@ -0,0 +1,14 @@
+package edu.comp373.model.facility;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FacilityTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
